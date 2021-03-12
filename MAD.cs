7175d8525d8ce9d636456122cfadf3a217596014
Meriem-elh <80535432+Meriem-elh@users.noreply.github.com>
2021-03-12 19:34:02 +0100

    Add files via upload

---
+++ MAD.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MAD
+    {
+        private double valeur;
+       
+        public MAD()
+        { }
+        public MAD(double v)
+        {
+            this.valeur = v;
+        }
+
+
+        public static MAD operator +(MAD somme, MAD solde)
+        {
+            MAD p = new MAD(0);
+            p.valeur = solde.valeur + somme.valeur;
+            return p;
+        }
+        public static bool operator >(MAD somme, MAD solde)
+        {
+            if (somme.valeur > solde.valeur)
+                return true;
+            else
+                return false;
+
+        }
+        public static bool operator >=(MAD somme, MAD solde)
+        {
+            if (somme.valeur >= solde.valeur)
+                return true;
+            else
+                return false;
+
+        }
+        public static bool operator <(MAD somme, MAD solde)
+        {
+            if (somme.valeur < solde.valeur)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <=(MAD somme, MAD solde)
+        {
+            if (somme.valeur <= solde.valeur)
+                return true;
+            else
+                return false;
+        }
+        public static MAD operator -(MAD somme, MAD solde)
+        {
+            MAD m = new MAD(0);
+            m.valeur = solde.valeur - somme.valeur;
+            return m;
+        }
+        public void afficher()
+        {
+            Console.WriteLine(this.valeur);
+        }
+    }
+}
+
+
+
+
