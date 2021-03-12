bc7a2be6509b2ada91c9ebf6865a23d0bc085d18
Meriem-elh <80535432+Meriem-elh@users.noreply.github.com>
2021-03-12 19:26:37 +0100

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Client
+    {
+        private string Nom;
+        private string Prenom;
+        private string Aresse;
+
+        public Client (string nom , string prenom,string adresse)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Aresse = adresse;
+
+
+        }
+        public void afficher()
+        {
+            Console.WriteLine("Votre nom  " + this.Nom + "Votre prenom  " +this.Prenom + "Votre adresse  " + this.Aresse);
+         
+        }
+
+
+
+
+    }
+
+}
